d93e1506f9a789cd1cbf99e44389a1793d256852
bpotet <bastien.potet@heig-vd.ch>
2020-06-03 10:37:54 +0200

    Change script.sh

--- docker-images/script.sh
+++ docker-images/script.sh
@@ -6,27 +6,15 @@ docker build -t res/apache_rp ./apache-reverse-proxy
 docker run -d --name static_1 res/apache_static	
 ip_static_1=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' static_1)":80"
 
-docker run -d --name static_2 res/apache_static
-ip_static_2=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' static_2)":80"
-
-docker run -d --name static_3 res/apache_static
-ip_static_3=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' static_3)":80"
-
-export STATIC_IP=${ip_static_1}";"${ip_static_2}";"${ip_static_3}
+export STATIC_IP=${ip_static_1}
 
 
 docker run -d --name dynamic_1 res/express_dynamic
 ip_dynamic_1=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dynamic_1)":3000"
 
-docker run -d --name dynamic_2 res/express_dynamic
-ip_dynamic_2=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dynamic_2)":3000"
+export DYNAMIC_IP=${ip_dynamic_1}
 
-docker run -d --name dynamic_3 res/express_dynamic
-ip_dynamic_3=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dynamic_3)":3000"
-
-export DYNAMIC_IP=${ip_dynamic_1}";"${ip_dynamic_2}";"${ip_dynamic_3}
-
-echo "IpAdresses statiques : $ip_static_1, $ip_static_2, $ip_static_3"
-echo "IpAdresses statiques : $ip_dynamic_1, $ip_dynamic_2, $ip_dynamic_3"
+echo "IpAdresses statiques : $ip_static_1"
+echo "IpAdresses statiques : $ip_dynamic_1"
 
 docker run -d -e STATIC_IP -e DYNAMIC_IP -p 8080:80 --name reverseProxy res/apache_rp
